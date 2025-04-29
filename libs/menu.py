23d33813248dfe71b7d8435fefc39e751b172e63
bonbon <mail@jan-schmitt.eu>
2025-04-29 21:42:46 +0200

    created own player class

--- libs/menu.py
+++ libs/menu.py
@@ -1,18 +1,13 @@
 import pygame
 from libs import tiles
-
+from libs.objects import player
 player_tiles = tiles.TileSet("data/images/player_spritesheet.png", 16, 16)
 
 def show_menu(Screen, Clock):
-    temp_player_sub_frame_count = 0
-    temp_player_frame_count = 0
-    temp_player_movement = 0
-    temp_player_pre_movement = 0
-    menu_part = 0
-    x = 288
-    y = 208
+    player_obj = player.Player(0,0,0,0,288,208)
     radius = 1
     menu_selection = 0
+    menu_part = 0
     run_menu = True
     radiusaddsize = 1
     while run_menu:
@@ -24,8 +19,8 @@ def show_menu(Screen, Clock):
         keys = pygame.key.get_pressed()
         match menu_part:
             case 0:
-                player_sprite = player_tiles.get_tile_scalled(temp_player_frame_count, (64,64))
-                Screen.blit(player_sprite, (x, y))
+                player_sprite = player_tiles.get_tile_scalled(player_obj.frame_count, (64,64))
+                Screen.blit(player_sprite, (player_obj.x, player_obj.y))
                 mask = pygame.Surface((640, 480), pygame.SRCALPHA)
                 mask.fill((0, 0, 0, 255))
                 pygame.draw.circle(mask, (0, 0, 0, 0), (320, 240), round(radius))
@@ -35,38 +30,38 @@ def show_menu(Screen, Clock):
                 if radius >= int((640**2 + 480**2)**0.5)+5:
                     menu_part = 1
             case 1:
-                temp_player_pre_movement = temp_player_movement
-                temp_player_movement = 0
+                player_obj.pre_movement = player_obj.movement
+                player_obj.movement = 0
                 if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
-                    y -= 4
-                    temp_player_movement = 1
+                    player_obj.y -= 4
+                    player_obj.movement = 1
                 elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
-                    y += 4
-                    temp_player_movement = 2
+                    player_obj.y += 4
+                    player_obj.movement = 2
                 else:
-                    temp_player_movement = 0
+                    player_obj.movement = 0
                 if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
-                    x -= 4
-                    temp_player_movement = 3
-                    player_sprite = pygame.transform.flip(player_tiles.get_tile_scalled(temp_player_frame_count, (64,64)), True, False)
+                    player_obj.x -= 4
+                    player_obj.movement = 3
+                    player_sprite = pygame.transform.flip(player_tiles.get_tile_scalled(player_obj.frame_count, (64,64)), True, False)
                 elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
-                    x += 4
-                    temp_player_movement = 4
-                    player_sprite = player_tiles.get_tile_scalled(temp_player_frame_count, (64,64))
-                if temp_player_pre_movement == temp_player_movement and temp_player_movement != 0:
-                    temp_player_sub_frame_count += 1
-                    print(temp_player_frame_count, temp_player_movement, temp_player_pre_movement, temp_player_sub_frame_count)
-                    if temp_player_sub_frame_count == 4:
-                        temp_player_frame_count += 1
-                        temp_player_sub_frame_count = 0
-                    if temp_player_frame_count == 7:
-                        temp_player_frame_count = 1
+                    player_obj.x += 4
+                    player_obj.movement = 4
+                    player_sprite = player_tiles.get_tile_scalled(player_obj.frame_count, (64,64))
+                if player_obj.pre_movement == player_obj.movement and player_obj.movement != 0:
+                    player_obj.sub_frame_count += 1
+                    print(player_obj.frame_count, player_obj.movement, player_obj.pre_movement, player_obj.sub_frame_count)
+                    if player_obj.sub_frame_count == 4:
+                        player_obj.frame_count += 1
+                        player_obj.sub_frame_count = 0
+                    if player_obj.frame_count == 7:
+                        player_obj.frame_count = 1
                 else:
-                    temp_player_frame_count = 0
-                    player_sprite = player_tiles.get_tile_scalled(temp_player_frame_count, (64,64))
-                    print(temp_player_frame_count, temp_player_movement, temp_player_pre_movement, temp_player_sub_frame_count)
-                    temp_player_sub_frame_count = 0
-                Screen.blit(player_sprite, (x, y))
+                    player_obj.frame_count = 0
+                    player_sprite = player_tiles.get_tile_scalled(player_obj.frame_count, (64,64))
+                    print(player_obj.frame_count, player_obj.movement, player_obj.pre_movement, player_obj.sub_frame_count)
+                    player_obj.sub_frame_count = 0
+                Screen.blit(player_sprite, (player_obj.x, player_obj.y))
 
         pygame.display.flip()
         Clock.tick(60)
---
+++ libs/objects/player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, sub_frame_count, frame_count, movement, pre_movement, x, y):
+        self.sub_frame_count = sub_frame_count
+        self.frame_count = frame_count
+        self.movement = movement
+        self.pre_movement = pre_movement
+        self.x = x
+        self.y = y
