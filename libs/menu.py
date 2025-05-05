4ba5681b532f7478c09bb7932517e372afd05598
bonbon <mail@jan-schmitt.eu>
2025-05-05 16:40:14 +0200

    moved player movement to player section

--- libs/menu.py
+++ libs/menu.py
@@ -4,7 +4,7 @@ from libs.objects import player
 player_tiles = tiles.TileSet("data/images/player_spritesheet.png", 16, 16)
 
 def show_menu(Screen, Clock):
-    player_obj = player.Player(0,0,0,0,288,208)
+    player_obj = player.Player(0,0,0,0,288,208,0, player_tiles)
     radius = 1
     menu_selection = 0
     menu_part = 0
@@ -19,8 +19,8 @@ def show_menu(Screen, Clock):
         keys = pygame.key.get_pressed()
         match menu_part:
             case 0:
-                player_sprite = player_tiles.get_tile_scalled(player_obj.frame_count, (64,64))
-                Screen.blit(player_sprite, (player_obj.x, player_obj.y))
+                player_obj.frame = player_obj.player_spritesheet.get_tile_scalled(player_obj.frame_count, (64,64))
+                Screen.blit(player_obj.frame, (player_obj.x, player_obj.y))
                 mask = pygame.Surface((640, 480), pygame.SRCALPHA)
                 mask.fill((0, 0, 0, 255))
                 pygame.draw.circle(mask, (0, 0, 0, 0), (320, 240), round(radius))
@@ -30,38 +30,8 @@ def show_menu(Screen, Clock):
                 if radius >= int((640**2 + 480**2)**0.5)+5:
                     menu_part = 1
             case 1:
-                player_obj.pre_movement = player_obj.movement
-                player_obj.movement = 0
-                if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
-                    player_obj.y -= 4
-                    player_obj.movement = 1
-                elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
-                    player_obj.y += 4
-                    player_obj.movement = 2
-                else:
-                    player_obj.movement = 0
-                if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
-                    player_obj.x -= 4
-                    player_obj.movement = 3
-                    player_sprite = pygame.transform.flip(player_tiles.get_tile_scalled(player_obj.frame_count, (64,64)), True, False)
-                elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
-                    player_obj.x += 4
-                    player_obj.movement = 4
-                    player_sprite = player_tiles.get_tile_scalled(player_obj.frame_count, (64,64))
-                if player_obj.pre_movement == player_obj.movement and player_obj.movement != 0:
-                    player_obj.sub_frame_count += 1
-                    print(player_obj.frame_count, player_obj.movement, player_obj.pre_movement, player_obj.sub_frame_count)
-                    if player_obj.sub_frame_count == 4:
-                        player_obj.frame_count += 1
-                        player_obj.sub_frame_count = 0
-                    if player_obj.frame_count == 7:
-                        player_obj.frame_count = 1
-                else:
-                    player_obj.frame_count = 0
-                    player_sprite = player_tiles.get_tile_scalled(player_obj.frame_count, (64,64))
-                    print(player_obj.frame_count, player_obj.movement, player_obj.pre_movement, player_obj.sub_frame_count)
-                    player_obj.sub_frame_count = 0
-                Screen.blit(player_sprite, (player_obj.x, player_obj.y))
+                player_obj.move(keys)
+                Screen.blit(player_obj.frame, (player_obj.x, player_obj.y))
 
         pygame.display.flip()
         Clock.tick(60)
--- libs/objects/player.py
+++ libs/objects/player.py
@@ -1,8 +1,65 @@
+from libs import tiles
+import pygame
+
 class Player:
-    def __init__(self, sub_frame_count, frame_count, movement, pre_movement, x, y):
+    def __init__(self, sub_frame_count, frame_count, movement, pre_movement, x, y, last_movement, spritesheet):
         self.sub_frame_count = sub_frame_count
         self.frame_count = frame_count
         self.movement = movement
         self.pre_movement = pre_movement
         self.x = x
         self.y = y
+        self.last_movement = last_movement
+        self.player_spritesheet = spritesheet
+        self.frame = self.player_spritesheet.get_tile_scalled(self.frame_count, (64,64))
+        self.switch_frame = 5
+        self.add_frame_count = 0
+    def move(self, keys):
+        self.pre_movement = self.movement
+        self.movement = 0
+        if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+            self.y -= 4
+            self.movement = 1
+            self.add_frame_count = 5
+            self.switch_frame = 5
+            self.last_movement = 1
+            self.frame = self.player_spritesheet.get_tile_scalled(self.frame_count+self.add_frame_count, (64,64))
+        elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+            self.y += 4
+            self.movement = 2
+            self.add_frame_count = 0
+            self.switch_frame = 5
+            self.last_movement = 2
+            self.frame = self.player_spritesheet.get_tile_scalled(self.frame_count+self.add_frame_count, (64,64))
+        else:
+            self.movement = 0
+        if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            self.x -= 4
+            self.movement = 3
+            self.last_movement = 3
+            self.add_frame_count = 10
+            self.switch_frame = 7
+            self.frame = pygame.transform.flip(self.player_spritesheet.get_tile_scalled(self.frame_count+self.add_frame_count, (64,64)), True, False)
+        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            self.x += 4
+            self.movement = 4
+            self.add_frame_count = 10
+            self.switch_frame = 7
+            self.frame = self.player_spritesheet.get_tile_scalled(self.frame_count+self.add_frame_count, (64,64))
+            self.last_movement = 4
+        if self.pre_movement == self.movement and self.movement != 0:
+            self.sub_frame_count += 1
+            print(self.frame_count+self.add_frame_count, self.movement, self.pre_movement, self.sub_frame_count)
+            if self.sub_frame_count == 4:
+                self.frame_count += 1
+                self.sub_frame_count = 0
+            print(self.add_frame_count+self.switch_frame)
+            if self.frame_count+self.add_frame_count >= self.add_frame_count+self.switch_frame:
+                self.frame_count = 1
+        else:
+            self.frame_count = 0
+            self.add_frame_count = 0
+            self.switch_frame = 0
+            self.frame = self.player_spritesheet.get_tile_scalled(self.frame_count, (64,64))
+            print(self.frame_count+self.add_frame_count, self.movement, self.pre_movement, self.sub_frame_count)
+            self.sub_frame_count = 0
